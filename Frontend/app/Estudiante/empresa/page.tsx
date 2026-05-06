ab41b82ccc535ea53daef32a10db13ebb4b7558c
jd-software <leandrohernandezjackdaniel@gmail.com>
2026-05-06 17:12:04 -0500

    feat: Modificando el estilo del sidebar, agrengado las routes de avence pea y empresa

---
+++ Frontend/app/Estudiante/empresa/page.tsx
@@ -0,0 +1,5 @@
+function page() {
+  return <div>Empresa</div>;
+}
+
+export default page;
--- Frontend/app/Estudiante/layout.tsx
+++ Frontend/app/Estudiante/layout.tsx
@@ -1,11 +1,13 @@
-import { SidebarProvider } from "@/components/ui/sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/appSidebar";
 import { TooltipProvider } from "@/components/ui/tooltip";
 
 const menuItems = [
   { title: "Dashboard", url: "/Estudiante/dashboard" },
-  { title: "Perfil", url: "/Estudiante/perfil" },
   { title: "Bolsa de Trabajo", url: "/Estudiante/bolsa" },
+  { title: "Empresa", url: "/Estudiante/empresa" },
+  { title: "Avance PEA", url: "/Estudiante/avance" },
+  { title: "Perfil", url: "/Estudiante/perfil" },
 ];
 
 export default function DashboardLayout({
@@ -16,10 +18,13 @@ export default function DashboardLayout({
   return (
     <TooltipProvider>
       <SidebarProvider>
-        <AppSidebar
-          menuItems={menuItems}
-        />
-        <main className="flex-1">{children}</main>
+        <AppSidebar menuItems={menuItems} />
+        <main className="w-full">
+          <header className="flex h-12 items-center px-4 border-b">
+            <SidebarTrigger />
+          </header>
+          {children}
+        </main>
       </SidebarProvider>
     </TooltipProvider>
   );
