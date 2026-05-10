bc6a979269e915286f8c243630beeedd78fa9020
jd-software <leandrohernandezjackdaniel@gmail.com>
2026-05-10 18:16:12 -0500

    Componentes creados SimpleProgressCard y ChekListCard, Usados en la pagina de bolsa, Page de bolsa realizado

--- Frontend/app/Estudiante/bolsa/page.tsx
+++ Frontend/app/Estudiante/bolsa/page.tsx
@@ -1,5 +1,51 @@
-function page() {
-  return <div>Bolsa de Trabajo</div>;
+import CheckListCard from "@/components/CheckListCard";
+import { Card } from "@/components/ui/card";
+import { Progress } from "@/components/ui/progress";
+import { SimpleProgressCard } from "@/components/SimpleProgressCard";
+
+import peaData from "@/data/peaData.json";
+
+function BolsaPage() {
+  return (
+    <div>
+      <div className="flex items-center justify-between text-3xl font-bold py-4">
+        <h1>Seguimiento del Plan Especifico de Aprendizaje</h1>
+      </div>
+      <section className="space-y-4">
+        <Card className="p-6 ">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-6">
+            <div className="col-span-full">
+              <h1 className="text-xl font-bold mb-2">
+                Progreso General del PEA
+              </h1>
+              <p className="text-sm mb-2">
+                Has completado el 4% de tu plan de aprendizaje
+              </p>
+              <Progress
+                value={4}
+                className="w-full h-2"
+                indicatorClassName="bg-black dark:bg-white"
+              />
+            </div>
+
+            {/* SECCIÓN INFERIOR: Cursos en columnas */}
+            <SimpleProgressCard
+              titulo="GESTORES DE ADMINISTRACIÓN WEB"
+              progreso={7}
+            />
+            <SimpleProgressCard titulo="SERVICIOS WEB" progreso={0} />
+          </div>
+        </Card>
+        <Card className="p-6">
+          {peaData.map((curso) => (
+            <div key={curso.id} className="px-6 py-4 overflow-visible">
+              <CheckListCard data={curso} />
+            </div>
+          ))}
+        </Card>
+      </section>
+    </div>
+  );
 }
 
-export default page;
+export default BolsaPage;
