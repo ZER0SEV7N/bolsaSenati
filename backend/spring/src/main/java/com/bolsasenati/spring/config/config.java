f7c7c03aaec500bf110847d6705b824598d1b91d
daniel singer07 <danielsinger07@hotmail.com>
2026-05-13 11:58:08 -0500

    Feat: JWT funcional

--- backend/spring/src/main/java/com/bolsasenati/spring/config/config.java
+++
@@ -1,5 +0,0 @@
-package com.bolsasenati.spring.config;
-
-public class config {
-
-}
--- backend/spring/src/main/java/com/bolsasenati/spring/services/jwtServices.java
+++ backend/spring/src/main/java/com/bolsasenati/spring/services/jwtServices.java
@@ -1,5 +1,70 @@
 package com.bolsasenati.spring.services;
 
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+import org.springframework.beans.factory.annotation.Value;
+
+//Servicio para manejar la generación y validación de tokens JWT
+@Service
 public class jwtServices {
 
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    //Metodo para generar un token a partir del email del usuario
+    public String generarToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    //metodo para validar el token
+    public boolean validarToken(String token){
+        try{
+            Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    //Metodo para extraer el email del token
+    public String extraerEmail (String token) {
+        return extraerClaim(token, Claims::getSubject);
+    }
+
+    //Metodo generico para extraer cualquier claim del token
+    private <T> T extraerClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claimsResolver.apply(claims);
+    }
+
+    //Metodo para obtener la clave de firma a partir del secret
+    private Key getSignInKey() {
+        byte[] keyBytes = secret.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
